0f6e15c758fe3ada13f75965f1eb124de8b7be44
彭付林 <pfl(*)2016>
2020-06-11 16:02:10 +0800

    1.细节修改

--- mylibrary/src/main/java/com/lzy/mylibrary/Readtest.java
+++ mylibrary/src/main/java/com/lzy/mylibrary/Readtest.java
@@ -3,11 +3,6 @@ package com.lzy.mylibrary;
 import android.util.Log;
 
 /**
- * $
- * 作   者 :彭付林
- * 邮   箱 :pengfl@kingchannels.com
- * 日   期 :2020/6/11
- * 描   述 :个人信息
  */
 public class Readtest {
 
